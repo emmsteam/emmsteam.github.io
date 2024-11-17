9a5e2bcd4c94e640ad1bae96037aed2258d16663
emmsteam <1797314820@qq.com>
2024-11-17 18:43:09 +0800

    Site updated: 2024-11-17 18:43:09

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731840038383";
+const VERSION = "1731840129909";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
